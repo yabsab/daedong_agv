8f7c719d85052ae0926f6e9597f8b89a3d7f93aa
yazz <sk8yabsab@gmail.com>
2021-07-20 09:51:23 +0900

    launch file

--- src/agv_main.cpp
+++ src/agv_main.cpp
@@ -5,11 +5,117 @@ struct i2c_rdwr_ioctl_data data_i2c;
 
 char data[8] = {0};
 uint16_t mbbuf[10];
-
-using namespace ros;
+int countYellow =0;
 
 modbus_t *mb;
 
+using namespace ros;
+
+int set_motor()
+{
+
+ try
+ {
+    mb = modbus_new_rtu("/dev/ttyUSB0", 115200, 'N', 8, 1);
+  
+   if (mb == NULL)
+    {
+     
+     //return -1;
+    }
+  
+  modbus_set_debug(mb,TRUE);
+
+   if (modbus_connect(mb) == -1)
+    {
+      
+      modbus_free(mb);
+     // return -1;
+      
+    }
+ }
+
+ catch(exception *e)
+ {
+    ROS_INFO("checking usb port");
+    //return -1;
+ }
+ 
+  
+  modbus_set_slave(mb,1);
+  modbus_write_register(mb,0x0078,0x0001);
+
+ //modbus_write_register(mb,0x0079,0x01F4); //500rpm
+
+  modbus_set_slave(mb,3);
+  modbus_write_register(mb,0x0078,0x0001);
+ 
+ //modbus_write_register(mb,0x0079,0xFE0C);//-500rpm
+}
+
+void forwardAgv()
+{
+ 
+  modbus_set_slave(mb,1);
+  modbus_write_register(mb,0x0079,0x00F4); //250rpm
+
+  modbus_set_slave(mb,3);
+  modbus_write_register(mb,0x0079,0xFF06);//-250rpm
+ 
+}
+
+void backwardAgv()
+{
+
+  modbus_set_slave(mb,3);
+  modbus_write_register(mb,0x0079,0x01F4); //500rpm
+
+  modbus_set_slave(mb,1);
+  modbus_write_register(mb,0x0079,0xFE0C);//-500rpm
+
+} 
+
+void stopAgv()
+{
+  
+  modbus_set_slave(mb,1);
+  modbus_write_register(mb,0x0079,0x0000); 
+
+  modbus_set_slave(mb,3);
+  modbus_write_register(mb,0x0079,0x0000);
+
+}
+
+void lefgAgv()
+{
+
+  modbus_set_slave(mb,1);
+  modbus_write_register(mb,0x0079,0x00FA);  //250 rpm
+
+  modbus_set_slave(mb,3);
+  modbus_write_register(mb,0x0079,0x0000);
+
+}
+
+void rightAgv()
+{
+
+  modbus_set_slave(mb,1);
+  modbus_write_register(mb,0x0079,0x0000); 
+
+  modbus_set_slave(mb,3);
+  modbus_write_register(mb,0x0079,0x00FA);
+  
+}
+
+void feedTheLivestock()
+ {
+
+  //  stopAgv();
+  //  Rate loop_rate(4);
+  //  forwardAgv();
+
+ }
 
 void set_device(int file)
 {
@@ -51,119 +157,93 @@ int main(int argc, char **argv){
 	  printf("Failed to open the bus \n");
    }
 
-   mb = modbus_new_rtu("/dev/ttyUSB0", 115200, 'N', 8, 1);
-  
-   if (mb == NULL)
-    {
-     
-      return -1;
-    }
-  
-  modbus_set_debug(mb,TRUE);
-
-  if (modbus_connect(mb) == -1)
-   {
-      
-      modbus_free(mb);
-      return -1;
-      
-   }
- modbus_set_slave(mb,1);
- modbus_write_register(mb,0x0078,0x0001);
- modbus_write_register(mb,0x0079,0x0000);
- //modbus_write_register(mb,0x0079,0x01F4); //500rpm
-
- modbus_set_slave(mb,3);
- modbus_write_register(mb,0x0078,0x0001);
- modbus_write_register(mb,0x0079,0x0000);
- 
- //modbus_write_register(mb,0x0079,0xFE0C);//-500rpm
-
   set_device(file);
-
+  set_motor();
 
   init(argc, argv, "datasender");
 	
   NodeHandle n;
   Publisher pub = n.advertise<std_msgs::String>("/message", 1000);
   Publisher sendcolordata = n.advertise<std_msgs::String>("/color", 1000);
-  
-  Rate loop_rate(10);
+  Publisher colorRed = n.advertise<std_msgs::Int32>("/redColor",1000);
+  Publisher colorGreen = n.advertise<std_msgs::Int32>("/greenColor",1000);
+  Publisher colorBlue = n.advertise<std_msgs::Int32>("/blueColor",1000);
 
-  int count = 0;   
+  Rate loop_rate(1000);
 
   while(ok())
-  {    
-
-  //   memset(mbbuf,0,10*2);//Zero
-  //  if(modbus_read_registers(mb,0,15,mbbuf)==1)
-  //   {
-	//    loop_rate.sleep();  
-  //    ros::spinOnce();   
-  //   }
-
-    // modbus_close(mb);
-    // modbus_free(mb);
-    
+   {    
 
     std_msgs::String msg;
     std_msgs::String colormsg;
     std_msgs::Int32 colorData;
+    std_msgs::Int32 redColorData;
+    std_msgs::Int32 greenColorData;
+    std_msgs::Int32 blueColorData;
+
     stringstream ss;
 
     read(file,data,8);
 	  int cData =(data[1] * 256 + data[0]);
 	  int red = (data[3] * 256 + data[2]);
+    ROS_INFO("red : %d", red);
+    redColorData.data = red;
+
 	  int green = (data[5] * 256 + data[4]);
-	  int blue =(data[7] * 256 + data[6]);
+    ROS_INFO("green : %d", green);
+	  greenColorData.data = green;
 
-    if(red > green && red > blue)
-    {
-      
-      ss <<"r";
-      msg.data = ss.str();
-      colormsg.data = ss.str();
-      ROS_INFO("msg : %s", msg.data.c_str());
-      ROS_INFO("color : %s", colormsg.data.c_str());
-
-    }else if (blue > red && blue > green)
+    int blue =(data[7] * 256 + data[6]);
+    ROS_INFO("blue : %d",blue);
+    blueColorData.data = blue;
+       
+    if(red > 3200 && red < 4900)
      {
-
-      ss <<"b";
-      msg.data = ss.str();
-      colormsg.data = ss.str();
-      ROS_INFO("msg : %s", msg.data.c_str());
-      ROS_INFO("color : %s", colormsg.data.c_str());
-
-     }else if (green > red && green > blue)
-     {
-
-      ss <<"g";
-      msg.data = ss.str();
-      colormsg.data = ss.str();
-      ROS_INFO("msg : %s", msg.data.c_str());
-      ROS_INFO("color : %s", colormsg.data.c_str());
+     
+      forwardAgv();
+      ROS_INFO("red red red");
 
      }
+    else if(blue >2000 && blue <2900 && green > 4700 && green <6600)
+     {
+      
+      for(int i=0; i <155; i++)
+       {
+         rightAgv();   
+         ROS_INFO("yellow yellow yellow");
+       }
+      
+     }
+    else if (green >600 && green <800 && blue >500 && blue <800)
+     {
+
+      feedTheLivestock();
+      ROS_INFO("black black black");
+
+     }
+    else if (red >610 && red < 800 && green > 1200 && green < 1800)
+     {
+
+      ROS_INFO("green green green");
+     
+     } 
+
+    else
+     {
+
+      stopAgv();
        
-    // ss << "r" ;
-    // msg.data = ss.str();
-    // ROS_INFO("%s", msg.data.c_str());  
-
-
-    //  colorData.data = red;
-    //  ROS_INFO("red %d", colorData.data);
-    // colordata.data = green;
-    // ROS_INFO("green %d",colordata.data);
-    // colordata.data = blue;   
-    // ROS_INFO("blue %d",colordata.data);
+     }
 
     pub.publish(msg);   
     sendcolordata.publish(msg);
+    colorRed.publish(redColorData);
+    colorGreen.publish(greenColorData);
+    colorBlue.publish(blueColorData);
     spinOnce();        
     loop_rate.sleep();
-    
-  }
+
+   }
   
   return 0;
 }
